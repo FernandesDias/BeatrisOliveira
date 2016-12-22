82899c0aea8ae2e6c6c4e07ba0e700b54c3d8655
Tiburski <rgtiburski@gmail.com>
2016-12-22 00:06:13 -0200

    Teste

--- BeatrisOliveira/GUI/frmFormularioPrincipal.Designer.cs
+++ BeatrisOliveira/GUI/frmFormularioPrincipal.Designer.cs
@@ -54,7 +54,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(446, 384);
+            this.ClientSize = new System.Drawing.Size(505, 384);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "frmFormularioPrincipal";
