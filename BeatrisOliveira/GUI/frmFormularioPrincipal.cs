1b7df716bbd3e120cf3b837b4ab817ed1052eda6
Tiburski <rgtiburski@gmail.com>
2016-12-21 22:26:52 -0200

    Adicionado button

--- BeatrisOliveira/GUI/frmFormularioPrincipal.cs
+++ BeatrisOliveira/GUI/frmFormularioPrincipal.cs
@@ -16,5 +16,10 @@ namespace GUI
         {
             InitializeComponent();
         }
+
+        private void frmFormularioPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
